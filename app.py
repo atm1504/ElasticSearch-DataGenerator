8faefb1d7ae1b83c358f07dbb9023373f7844f86
Amartya Mondal <atm1504.in@gmail.com>
2021-12-07 21:20:06 +0530

    Make up structure

--- app.py
+++ app.py
@@ -1,25 +1,44 @@
 import json
 from collections import defaultdict
 from utility import isKeyPresent, getKeyValue
-from constants import DATA_FILE
+import constants as cnst
+from dotenv import load_dotenv
+import os
+from os.path import join, dirname
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+# Extract data from env file
+data_file_name=os.environ.get(cnst.DATA_FILE_NAME)
 
 
 """ 
-Extract features from the 
+Extract features from the data file
 """
-json_file = open(DATA_FILE)
+json_file = open(data_file_name)
 json_data=json.load(json_file)
-# print(json_data[0])
 
 channel_wise_data=defaultdict(list)
 
 for data in json_data:
     ## Extracting data
-    source=getKeyValue(data, "_source")
+    source=getKeyValue(data,cnst.SOURCE_)
     if not source:
         continue
-    channel = getKeyValue(source,  "channel")
-    creative_id=getKeyValue(source, "creative_id")
-    channel_wise_data[channel].append(creative_id)
+    channel = getKeyValue(source,  cnst.CHANNEL)
+    # creative_id=getKeyValue(source, cnst.CREATIVE_ID)
+
+    temp=dict()
+    temp[cnst.CREATIVE_ID]=getKeyValue(source, cnst.CREATIVE_ID)
+    temp[cnst.AGENCY_ID]=getKeyValue(source, cnst.AGENCY_ID)
+    temp[cnst.LINE_ID]=getKeyValue(source, cnst.LINE_ID)
+    temp[cnst.ADVERTISER_ID]=getKeyValue(source, cnst.ADVERTISER_ID)
+    temp[cnst.ACCOUNT_ID]=getKeyValue(source, cnst.ACCOUNT_ID)
+    temp[cnst.CAMPAIGN_ID]=getKeyValue(source, cnst.CAMPAIGN_ID)
+
+
+    channel_wise_data[channel].append(temp)
 
 print(channel_wise_data)
+
---
+++ structure.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+@dataclass
+class Structure:
+    date:str
+    conversions:float
+    ingestion_time:str
+    channel:str
+    agency_id:int
+    impressions:int
+    line_id:int
+    creative_id:int
+    video_view_complete:int
+    advertiser_id:int
+    account_id:int
+    job_id:int
+    spend:int
+    video_view_25p:int
+    clicks:int
+    video_view_75p:int
+    campaign_id:int
+    video_view_50p:int
+    
+
